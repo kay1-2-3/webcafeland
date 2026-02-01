9d1f6ec124dd689ebc8e3d92db2ab13ea2fd6cfd
kay1-2-3 <kunalkumar787@gmail.com>
2026-02-01 07:41:50 -0800

    Security Features with .env file

--- my-react-app/src/firebase.js
+++ my-react-app/src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBOrNMV3ZSSX_ozcl7MaVtLoe7mPMPsK44",
-  authDomain: "webcafelanding-a9b98.firebaseapp.com",
-  projectId: "webcafelanding-a9b98",
-  storageBucket: "webcafelanding-a9b98.firebasestorage.app",
-  messagingSenderId: "555662021339",
-  appId: "1:555662021339:web:bd1e810c46b70f0f26b2fe",
-  measurementId: "G-RED7Z95G1S"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
