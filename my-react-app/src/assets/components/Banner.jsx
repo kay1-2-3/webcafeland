07718bcbd997a3a6dff50a541d74095abb7cdd92
kay1-2-3 <kunalkumar787@gmail.com>
2026-02-15 11:39:10 -0800

    Updates

--- my-react-app/src/assets/components/Banner.jsx
+++ my-react-app/src/assets/components/Banner.jsx
@@ -4,7 +4,7 @@ import "./Banner.css";
 const Banner = () => {
   return (
     <div className="banner">
-      <div>Welcome to Webcafe AI</div>
+      <div id="banner-title">Welcome to Webcafe AI</div>
       <div>Curently looking to secure funding, please consider supporting us.</div>
       <img src="/cup192.png" alt="Webcafe" className="banner-logo" />
     </div>
